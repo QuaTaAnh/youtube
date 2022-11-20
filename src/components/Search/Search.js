a20071e4fc6ba339d4d4b9c84eebf52bfa26bd3f
Your Name <you@example.com>
2022-11-21 04:29:58 +0700

    Header

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,5 +1,23 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './Search.module.scss';
+
+const cx = classNames.bind(styles);
+
+
 function Search() {
-    return <h2>Search</h2>;
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('search')}>
+                <input
+                className={cx('input')}
+                />
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </div>
+        </div>
+    );
 }
 
 export default Search;
